b4c7747fdbc862c7d894ec8d1093d943d73dcf23
jogutierrez99 <64194024+jogutierrez99@users.noreply.github.com>
2024-12-05 22:00:33 +0100

    login/ register --> falta realizar pruebas con el login

--- API_Eventos_Deportivos/index.js
+++ API_Eventos_Deportivos/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const connectDB = require("./src/utils/db_mongo");
 //Exportar de routes
-//const routes = require("./src/api/routers/routes");
+const routes = require("./src/api/routers/routes");
 connectDB();
 
 const server = express();
@@ -10,7 +10,7 @@ server.use(express.json());
 //const port = 3500;
 const PORT = process.env.PORT
 
-//server.use("/", routes);
+server.use("/", routes);
 
 server.listen( PORT , ()=>{
     console.log(`server running port http://localhost:${PORT}`);
